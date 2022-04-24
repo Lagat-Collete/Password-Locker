9b6776ffec37231bc8d24ad6d20090e90a7be668
colletemine <jepngetichcolleta@gmail.com>
2022-04-24 13:52:51 +0300

    complete test files

--- password.py
+++ password.py
@@ -1,3 +1,6 @@
+
+
+
 class Password:
   '''
 class that generates new instances of password
@@ -16,3 +19,25 @@ class that generates new instances of password
     '''
 
     Password.password_list.append(self)
+  
+#delete method
+  def delete_password(self):
+    '''
+    delete_password method deletes a saved password from the password_list
+    '''
+    Password.password_list.remove(self)
+
+    #find method
+  @classmethod
+  def find_by_plot(cls,plot):
+    '''
+    method that takes  in plot and returns a password that matches the plot
+    Args:
+        plot:plot to search
+    Returns:
+        passord of that plot matches
+    '''
+
+    for password in cls.password_list:
+      if password.plot == plot:
+        return password
--- password_test.py
+++ password_test.py
@@ -1,3 +1,5 @@
+
+
 import unittest # Importing the unittest module
 from password import Password # Importing the password class
 
@@ -15,6 +17,14 @@ class TestPassword(unittest.TestCase):
     '''
     self.new_password = Password("facebook","face1") #create password object
     
+  def tearDown(self):
+    '''
+    tearDown method that does clean up after each test case has run.
+    '''
+
+    Password.password_list = []
+
+
   def test_init(self):
     '''
     test_init test case to test if the object is initialized properly
@@ -30,7 +40,56 @@ class TestPassword(unittest.TestCase):
     '''
     self.new_password.save_password()  #saving the new password
     self.assertEqual(len(Password.password_list),1)
-    
+
+#Third Test-for multiple passwords
+  def test_save_multiple_password(self):
+    '''
+    test_save_multiple_password to check if we can save multiple password
+            objects to our password_list
+
+    '''
+    self.new_password.save_password()
+    test_pass = Password("wi-fi","wifi1") # new password
+    test_pass.save_password()
+    self.assertEqual(len(Password.password_list),2)
+
+    #Fourth Test-for delete
+  def test_delete_password(self):
+    '''
+    test delete action
+    '''
+    self.new_password.save_password()
+    test_pass = Password('wi-fi','wifi1')
+    test_pass.save_password()
+    self.new_password.delete_password()
+    self.assertEqual(len(Password.password_list),1)
+
+    #Fifth-test for finding
+  def test_find_password(self):
+    '''
+    This checks if we can find password using plot name
+    '''
+    self.new_password.save_password()
+    test_pass = Password('wi-fi','wifi1')
+    test_pass.save_password()
+    found_password = Password.find_by_plot('wi-fi')
+    self.assertEqual(found_password.plot, test_pass.plot)
+
+    #sixth-Test -For existence
+  def test_exists(self):
+    '''
+    returns a true/false value depending on value presence on search
+    '''
+
+    self.new_password.save_password()
+    test_pass = Password('wi-fi','wifi1')
+    test_pass.save_password()
+    plot_exist = Password.find_by_plot('wi-fi')
+    self.assertTrue(plot_exist)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
